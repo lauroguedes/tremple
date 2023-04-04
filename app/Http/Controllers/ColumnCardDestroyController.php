72f9785a0b5c97427cb5a912a64014711bfd5381
Lauro Guedes <leowgweb@gmail.com>
2023-04-04 18:22:29 -0300

    feat: create actions for components

---
+++ app/Http/Controllers/ColumnCardDestroyController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Card;
+use App\Models\Column;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class ColumnCardDestroyController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Column $column, Card $card): RedirectResponse
+    {
+        $card->delete();
+
+        return back();
+    }
+}
